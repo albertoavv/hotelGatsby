13fce243b464c660e2c95d831ed43db315df865e
Alberto <albertoavv@hotmail.es>
2020-11-28 23:59:33 -0400

    hotel gatsby

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,53 +1,69 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
+import React from 'react';
+import Helmet from 'react-helmet';
+import {Global, css } from '@emotion/core';
+import Header from './header';
+import Footer from './footer';
+import useSeo from '../hooks/use-seo';
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+const Layout = ({children}) => {
 
-import Header from "./header"
-import "./layout.css"
+  const seo = useSeo();
+  //console.log(seo);
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+  const { siteName, fallbackSeo: { description, title }} = seo; 
+  
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer style={{
-          marginTop: `2rem`
-        }}>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
+      <Global 
+        styles={css`
+        html {
+          font-size:62.5%;
+          box-sizing: border-box;
+        }
+        *, *:before, *:after{
+          box-sizing: inherit;
+        }
+
+        h1,h2,h3 {
+          margin: 0;
+          line-height: 1.5;
+        }
+        h1,h2{
+          font-family: 'Roboto', serif;
+        }
+          h3{
+            font-family: 'PT Sans', sans-serif;
+          }
+          ul{
+            list-style: none;
+            margin: 0;
+            padding:0;
+          }
+          body {
+            font-size: 18px;
+            font-size: 1.6rem;
+            line-height: 1.5;
+            font-family: 'PT Sans', sans-serif;
+          }
+        `}
+      />
+
+    <Helmet>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+      <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Roboto:ital,wght@0,100;0,400;1,300&display=swap" rel="stylesheet" />
+
+    </Helmet>
+    <Header />
+        { children }
+        <Footer 
+          title={title}
+        />
     </>
-  )
-}
 
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+  );
+};
 
-export default Layout
+export default Layout;
